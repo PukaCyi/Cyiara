a0a374c896a91747b7fa0daa3700b9ecbabbdd6a
PukaCyi <149914478+PukaCyi@users.noreply.github.com>
2023-12-16 21:49:06 -0600

    s p a c e

--- global/text/global.js
+++ global/text/global.js
@@ -1,16 +1,14 @@
 // hahaha haxe trace !! NO WAY!! 
 var trace = function(txt, ty) {
     zType = { warn: "Warning", note: "Notice" };
-
     zColor = { warn: "#ff0019", note: "#ffa600", };
-
     zStyle = `color: ${zColor[ty]}; font-weight: bold; font-family: "Arial"; font-size: 13px;`;
 
     console.log(`%c[ ${zType[ty]} ] %c${txt}`, zStyle, '');
 
     /** 
      * Usage:
-     * Input: trace("Banami is cute but Aori...","warn") - Output: ** [ Warning ] Banami is cute but Aori...
+     * Input: trace("Banami is cute but Aori...","warn") - Output: ** [ Warning ] ** Banami is cute but Aori...
      */
 };
 
