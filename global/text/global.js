c77facce0c226e770eac25480611502bbf7cd468
Banami <149914478+PukaCyi@users.noreply.github.com>
2024-02-03 21:45:07 +0000

    improvements maybe

--- global/text/global.js
+++ global/text/global.js
@@ -1,23 +1,26 @@
 // hahaha haxe trace !! NO WAY!! 
-const trace = function(txt, ty) {
-    ver = { warn: "Warning", note: "Notice" };
-    hex = { warn: "#ff0019", note: "#ffa600", };
-    pat = `color: ${hex[ty]}; font-weight: bold; font-family: "Arial"; font-size: 13px;`;
+const trace = function (txt, ty, ...nono) {
+    const ver = { warn: "Warning", note: "Notice" };
+    const hex = { warn: "#ff0019", note: "#ffa600" };
+    const pat = `color: ${hex[ty]}; font-weight: bold; font-family: "Arial"; font-size: 13px;`;
 
     /* Error Catcher */
-    if (!(ty in ver)) {
+    if (nono.length > 0 || !(ty in ver)) {
         trace("You didn't run the trace command correctly. Please review the Help Guide for tracing below.", "warn");
         traceHelper();
-    }
-    else {
+    } else {
         console.log(`%c[ ${ver[ty]} ] %c${txt}`, pat, '');
+    }
+}
 
-    }    
+// I don't know how to make the handler show an error message - so have this "Something went wrong" message instead.
+window.onerror = function() {
+    trace("An error has occurred while attempting to perform an action.","warn");
 };
 
-var traceHelper = function() {
-    trace('\nTrace Doc\n--------------\n2 types: "warn", "note"', "note"); 
-    trace("Banami is cute but Aori...", "warn"); //hueh 
+const traceHelper = function () {
+    trace('\nTrace Doc\n--------------\n2 types: "warn", "note"', "note");
+    trace("Banami is cute but Aori...", "warn"); //hueh
 };
 
 errorPage = document.getElementsByClassName('error-page');
@@ -30,9 +33,8 @@ document.addEventListener("DOMContentLoaded", function() {
     trace("Site fully loaded! :D", "note");
 });
 
-
 function hikkiomori() {
-    document.cookie = omo; 
+    document.cookie = omo;
 }
 
 let bossRush = false;
@@ -40,7 +42,7 @@ let bossRush = false;
 function hikki() {
     if (!bossRush && document.cookie.includes('omo')) {
         bossRush = true;
-        hueh = new Audio('https://pukacyi.github.io/Cyiara/global/assets/music/hikkomori.mp3');
+        const hueh = new Audio('https://pukacyi.github.io/Cyiara/global/assets/music/hikkomori.mp3');
         hueh.play();
         hueh.addEventListener('ended', function() {
             this.currentTime = 0;
