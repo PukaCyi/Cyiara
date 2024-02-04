e7284c59739133436273218649130afadff5defd
Banami <149914478+PukaCyi@users.noreply.github.com>
2024-02-04 18:22:32 +0000

    this is like the 6th time I have to open the stupid editor

--- global/text/paths.js
+++ global/text/paths.js
@@ -9,39 +9,16 @@
 currentChannel = window.location.href.toLowerCase()
 miiChannel = location.href 
 
-/** defining trace function for paths.js only */
-const trace = function (txt, ty, ...nono) {
-  const ver = { warn: "Warning", note: "Notice" };
-  const hex = { warn: "#ff0019", note: "#ffa600" };
-  const pat = `color: ${hex[ty]}; font-weight: bold; font-family: "Arial"; font-size: 13px;`;
-
-  /* Error Catcher */
-  if (nono.length > 0 || !(ty in ver)) {
-      trace("You didn't run the trace command correctly. Please review the Help Guide for tracing.", "warn");
-  } else {
-      console.log(`%c[ ${ver[ty]} ] %c${txt}`, pat, '');
-  }
-}
-
 if (currentChannel === "https://pukacyi.github.io/cyiara/aboutme" || currentChannel === "https://pukacyi.github.io/cyiara/about") {
     miiChannel = "https://pukacyi.github.io/Cyiara/pages/aboutme";
-    trace("Redirecting.", "note");
-  } else 
-  if (currentChannel === "https://pukacyi.github.io/cyiara/dni") {
+} else if (currentChannel === "https://pukacyi.github.io/cyiara/dni") {
     miiChannel = "https://pukacyi.github.io/Cyiara/pages/dni";
-    trace("Redirecting.", "note");
-  } else 
-  if (currentChannel === "https://pukacyi.github.io/cyiara/interests") {
+} else if (currentChannel === "https://pukacyi.github.io/cyiara/interests") {
     miiChannel = "https://pukacyi.github.io/Cyiara/pages/interests";
-    trace("Redirecting.", "note");
-  } else 
-  if (
+} else if (
     currentChannel === "https://pukacyi.github.io/cyiara/wdy" || currentChannel === "https://pukacyi.github.io/cyiara/mii" || currentChannel === "https://pukacyi.github.io/cyiara/henry" || currentChannel === "https://pukacyi.github.io/cyiara/austin" || currentChannel === "https://pukacyi.github.io/cyiara/eteled"
-  ) {
+) {
     miiChannel = "https://pukacyi.github.io/Cyiara/wii";
-    trace("Redirecting.", "note");
-  } else 
-  if (currentChannel === "https://pukacyi.github.io/cyiara/cyinora") {
+} else if (currentChannel === "https://pukacyi.github.io/cyiara/cyinora") {
     miiChannel = "https://twitter.com/NightCyi";
-    trace("Redirecting.", "note");
 }
