a7596ab47c7e557c6dbb1086c7f5fb2a03da640a
Banami <149914478+PukaCyi@users.noreply.github.com>
2024-02-04 16:40:37 +0000

    stuff

---
+++ global/text/paths.js
@@ -0,0 +1,40 @@
+/** 
+    * This sucks. I'll try finding something.
+    
+if (location.href = "https://pukacyi.github.io/Cyiara/aboutme") {
+    location.href = "https://pukacyi.github.io/pages/aboutme";
+    trace("Redirecting.","info")
+} else {
+    // do nothing idiot.
+}
+
+if (location.href = "https://pukacyi.github.io/Cyiara/about") {
+    location.href = "https://pukacyi.github.io/pages/aboutme"
+    trace("Redirecting.","info")
+} else {
+    // do nothing idiot.
+}
+
+if (location.href = "https://pukacyi.github.io/Cyiara/dni") {
+    location.href = "https://pukacyi.github.io/Cyiara/pages/dni"
+    trace("Redirecting.","info")
+} else {
+    // do nothing idiot.
+}
+
+if (location.href = "https://pukacyi.github.io/Cyiara/interests") {
+    location.href = "https://pukacyi.github.io/Cyiara/pages/interests"
+    trace("Redirecting.","info")
+}
+
+if (location.href = "https://pukacyi.github.io/Cyiara/wdy") {
+    location.href = "https://pukacyi.github.io/Cyiara/pages/wii"
+    trace("Redirecting.","info")
+}
+
+if (location.href = "https://pukacyi.github.io/Cyiara/Cyinora") {
+    location.href = "https://twitter.com/NightCyi"
+    trace("Redirecting.","info")
+}
+
+**/
